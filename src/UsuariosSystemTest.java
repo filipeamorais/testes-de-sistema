72c8490dbd0e5d718dd8bebecdf63d1344d7e4b4
Filipe de Andrade Morais <afilipem@gmail.com>
2017-11-26 22:03:02 -0200

    Aula 3 Completa - Page Objects

--- src/UsuariosSystemTest.java
+++ src/UsuariosSystemTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
@@ -10,12 +11,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class UsuariosSystemTest {
 	
 	private FirefoxDriver driver;
+	private UsuariosPage usuarios;
 	
 	@Before
 	public void inicializa() {
 		// por ser uma nova versão preciso setar o webdrive
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\afilipem\\Documents\\Instaladores\\Selenium\\geckodriver-v0.19.1-win64\\geckodriver.exe");
 		driver = new FirefoxDriver();
+		this.usuarios = new UsuariosPage(driver);
 	}
 
 	@After
@@ -25,12 +28,15 @@ public class UsuariosSystemTest {
 	
 	@Test
 	public void deveAdicionarUmUsuario() {
-		/*FirefoxDriver driver;
+		/* Nova Implentação 26/11/17
+		// Outra implementacao antiga
+		FirefoxDriver driver;
 		
 		// por ser uma nova versão preciso setar o webdrive
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\afilipem\\Documents\\Instaladores\\Selenium\\geckodriver-v0.19.1-win64\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		*/
+		// Outra implementacao antiga
+		
 		// acessa o site do google
 		driver.get("http://localhost:8080/usuarios/new");
 		
@@ -57,7 +63,10 @@ public class UsuariosSystemTest {
 		assertTrue(achouEmail);
 		
 		//driver.close();
-
+		Nova Implentação 26/11/17*/
+		usuarios.visita();
+		usuarios.novo().cadastra("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br");
+		assertTrue (usuarios.existeNaListagem("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br"));
 	}
 	
 	@Test
@@ -67,7 +76,7 @@ public class UsuariosSystemTest {
 		// por ser uma nova versão preciso setar o webdrive
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\afilipem\\Documents\\Instaladores\\Selenium\\geckodriver-v0.19.1-win64\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		*/
+		
 		// acessa o site do google
 		driver.get("http://localhost:8080/usuarios/new");
 		
@@ -86,6 +95,11 @@ public class UsuariosSystemTest {
 		assertTrue(driver.getPageSource().contains("Nome obrigatorio!"));
 		
 		//driver.close();
+		Nova implementação 26/11/17*/
+		usuarios.visita();
+		NovoUsuarioPage form = usuarios.novo();
+		form.cadastra("", "ronaldo2009@terra.com.br");
+		assertTrue(form.validacaoDeNomeObrigatorio());
 	}
 
 	@Test
@@ -129,6 +143,16 @@ public class UsuariosSystemTest {
 		
 		// Função JUnit para verificação
 		assertTrue(driver.getPageSource().contains("Nome:"));
-
+	}
+	
+	@Test
+	public void excluirUmUsuario() {
+		// adicionando um usuario qualquer
+		
+		usuarios.visita();
+		usuarios.novo().cadastra("Ronaldo Luiz de Albuquerque1", "ronaldo2009@terra.com.br1");
+		usuarios.deletaUsuarioNaPosicao(1);
+		
+		assertFalse(usuarios.existeNaListagem("Ronaldo Luiz de Albuquerque1", "ronaldo2009@terra.com.br1"));
 	}
 }
