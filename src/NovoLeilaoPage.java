535d9d0f939a5be0d35efeae75a1f28ec355d079
Filipe de Andrade Morais <afilipem@gmail.com>
2017-12-03 22:49:12 -0200

    Novas alterações avançando na aula 4

--- src/NovoLeilaoPage.java
+++ src/NovoLeilaoPage.java
@@ -1,19 +1,44 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class NovoLeilaoPage {
 	
 	private WebDriver driver;
+	
 	public NovoLeilaoPage(WebDriver driver) {
 		this.driver = driver;
 	}
 
-	WebElement nome = driver.findElement(By.name("leilao.nome"));
-	WebElement valor = driver.findElement(By.name("leilao.valorInicial"));
+	public void preenche (String nome, double valor, String usuario, boolean usado) {
+	WebElement nome1 = driver.findElement(By.name("leilao.nome"));
+	WebElement valor1 = driver.findElement(By.name("leilao.valorInicial"));
 	
-	nome.sendKeys("Dono do Produto");
-	valor.sendKeys("123,45");
+	nome1.sendKeys(nome);
+	valor1.sendKeys(String.valueOf(valor));
+	
+	WebElement combo = driver.findElement(By.name("leilao.usuario.id"));
+    Select cbUsuario = new Select(combo);
+    cbUsuario.selectByVisibleText(usuario);
+
+    if(usado) {
+        WebElement ckUsado = driver.findElement(By.name("leilao.usado"));
+        ckUsado.click();
+    }
+
+    nome1.submit();
+	
+	}
+	
+	public boolean validacaoDeProdutoApareceu() {
+        return driver.getPageSource().contains("Nome obrigatorio!");
+    }
+
+    public boolean validacaoDeValorApareceu() {
+        return driver.getPageSource()
+                .contains("Valor inicial deve ser maior que zero!");
+    }
 	
 	
 }
